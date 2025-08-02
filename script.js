437dc7c94730f86ebd51090870f99a0399a9a086
Mary mi <mmarymmiler@gmail.com>
2025-08-02 15:40:37 +0300

    Условия

--- script.js
+++ script.js
@@ -1,44 +1,112 @@
-// 1. Напиши код, который решает примеры с картинки выше. Результаты округлить;
-const a = 2;
-console.log(a * a + a);
+// 1. Напиши программу, которая проверяет число и выводит сообщение, является ли 
+// оно положительным, отрицательным или нулем;
 
-const numberOne = 54;
-const numberTwo = 16;
-const answer = (Math.sin(numberOne) * Math.cos(numberTwo)) ** a;
-console.log(Math.round(answer * 1000) / 1000);
+const num = 35;
 
-const answer2 = (((16 * Math.sqrt(13.2 * 71.9)) / (2.4 / (7 ** 4))) + (3 ** Math.sqrt(49))) * (2 **7);
-console.log(Math.round(answer2));
-
-/*2. Есть несколько числовых переменных (задай их самостоятельно). С помощью арифметических операторов 
-и операторов сравнения определи, четное это число или нет;*/
-let number1 = 256;
-let number2 = 15;
-let number3 = 82;
-
-if (number1 % 2 === 0) {
-    console.log(number1 + ' - чётное число')
-} else (
-    console.log(number1 + ' - нечётное число')
-);
-if (number2 % 2 === 0) {
-    console.log(number2 + ' - чётное число')
-} else (
-    console.log(number2 + ' - нечётное число')
-);
-if (number3 % 2 === 0) {
-    console.log(number3 + ' - чётное число')
-} else (
-    console.log(number3 + ' - нечётное число')
-);
-
-/* 3. Напиши программу, которая проверяет, если переменная name пуста или не задана, то в консоль должно выводиться
- сообщение "Hello, Guest!". Если переменная name содержит значение, то программа должна вывести сообщение
-  "Hello, [name]!", где [name] — это значение переменной. */
-let name1 ='Гаврила';
-
-if (!name1){
-    console.log('Hello, Guest!')
-} else{
-    console.log('Hello, ' + name1 + '!')
+if (num === 0){
+    console.log('Ноль');
+} else if (num > 0) {
+    console.log('Положительное');
+} else {
+    console.log('Отрицательное');
 }
+
+//2. Напиши программу, которая запрашивает у пользователя его рост и вес, на основании 
+// этих данных вычисляет ИМТ, и выводит сообщение о том, в каком диапазоне находится его ИМТ;
+
+const height = prompt('Введите ваш рост, м:');
+const weight = prompt('Введите ваш вес, кг:');
+
+let imt = weight / (height ** 2);
+
+console.log('Ваш ИМТ: ' + imt);
+
+//3. Используй оператор switch, чтобы вывести название месяца на основе введенного пользователем числа (1-12);
+const month = 10;
+let monthName;
+
+switch (month) {
+    case 1:
+        monthName = 'Январь';
+        break;
+    case 2:
+        monthName = 'Февраль';
+        break;
+    case 3:
+        monthName = 'Март';
+        break;
+    case 4:
+        monthName = 'Апрель';
+        break;
+    case 5:
+        monthName = 'Май';
+        break;
+    case 6:
+        monthName = 'Июнь';
+        break;
+    case 7:
+        monthName = 'Июль';
+        break;
+    case 8:
+        monthName = 'Август';
+        break;
+    case 9:
+        monthName = 'Сентябрь';
+        break;
+    case 10:
+        monthName = 'Октябрь';
+        break;
+    case 11:
+        monthName = 'Ноябрь';
+        break;
+    case 12:
+        monthName = 'Декабрьь';
+        break;
+    default:
+        monthName = 'Неверный месяц';
+}
+
+console.log(monthName); 
+
+//4. Придумай свое условие и опиши его в помощью оператора switch/case.
+// Пользователь вводит оценку 1-10, ему выводится её значение
+
+const point = 10;
+let pointName;
+
+switch (point) {
+    case 1:
+        pointName = 'Неудовлетворительно';
+        break;
+    case 2:
+        pointName = 'Почти удовлетворительно';
+        break;
+    case 3:
+        pointName = 'Удовлетворительно';
+        break;
+    case 4:
+        pointName = 'Весьма удовлетворительно';
+        break;
+    case 5:
+        pointName = 'Хорошо';
+        break;
+    case 6:
+        pointName = 'Весьма хорошо';
+        break;
+    case 7:
+        pointName = 'Очень хорошо';
+        break;
+    case 8:
+        pointName = 'Почти отлично';
+        break;
+    case 9:
+        pointName = 'Отлично';
+        break;
+    case 10:
+        pointName = 'Превосходно';
+        break;
+    default:
+        monthName = 'Неверный балл';
+}
+
+console.log(point + ' - ' + pointName);
